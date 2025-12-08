8b41f43979cc7e8a2b481cc3d4b430e7c43346e4
user1987128 <dewopasuka69@gmail.com>
2025-12-08 18:34:20 +0700

    Commit message for your changes. Lines starting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,42 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
+
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    </head>
+    
+</head>
 <body>
 
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('marketplace') }}">Marketplace</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cart.index') }}">Cart</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('user.guide') }}">User Guide</a>
-                    </li>
-                    @auth
+    <div id="app">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('orders.index') }}">My Orders</a>
+                            <a class="nav-link" href="{{ route('marketplace') }}">Marketplace</a>
                         </li>
-                    @endauth
-                </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('cart.index') }}">Cart</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.guide') }}">User Guide</a>
+                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('orders.index') }}">My Orders</a>
+                            </li>
+                        @endauth
+                    </ul>
                 </div>
-        </nav>
-    </header>
+            </nav>
+        </header>
 
-    <main class="container mt-4">
-        <h1>Welcome to {{ config('app.name', 'Laravel') }}</h1>
-        <p>This is the main content area of the application.</p>
-    </main>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 
     <script src="{{ mix('js/app.js') }}"></script>
-    </body>
+</body>
 </html>
